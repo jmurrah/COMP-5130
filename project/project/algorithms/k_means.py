30db1b9d3bd0b650ef91be8a6c3c148457b28ef3
jmurrah <jacobhmurrah@gmail.com>
2024-06-10 14:48:45 -0500

    Restructure project to allow for multiple algorithms

---
+++ project/project/algorithms/k_means.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+
+def transform_data(data: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def k_means(k: int, data: pd.DataFrame) -> tuple[list[int], np.ndarray]:
+    pass
---
+++ project/project/main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import argparse
+import project.algorithms as algorithms
+
+def convert_to_dataframe(file_path: str) -> pd.DataFrame:
+    with open(file_path, "r") as file:
+        raw_data = file.readlines()
+
+    data = {"from-node-id": [], "to-node-id": []}
+
+    for row in raw_data[4:]:
+        data["from-node-id"].append(row.split("\t")[0].strip())
+        data["to-node-id"].append(row.split("\t")[1].strip())
+
+    return pd.DataFrame(data)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo", type=str, help="Algorithm to run")
+    algorithm = parser.parse_args().algo
+
+    arxiv_small_dataset = convert_to_dataframe("./project/datasets/CA-GrQc.txt")
+    dblp_large_dataset = convert_to_dataframe("./project/datasets/com-dblp.ungraph.txt")
+
+    
+    if algorithm == "k-means":
+        algorithms.k_means.main()
